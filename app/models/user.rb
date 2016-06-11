c174c86abd51c8b1f3217cb630cd83f58aac3b88
Ziv Zamechek <ziv921@yahoo.com>
2016-06-11 14:37:03 -0400

    styling navigation bar and links

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ActiveRecord::Base
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Annonymous"
+  end
+
   def can_add_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_added?(ticker_symbol)
   end
